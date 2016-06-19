91d17759c7dd34979dc22dc1973c0dcdb0cb4373
VamSrik <sri.chandu53@gmail.com>
2016-06-19 14:03:03 +0530

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php
+
+$target_dir = "/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+var_dump($_FILES["file"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+?>
